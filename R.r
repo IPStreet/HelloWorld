2ab7acef36786b4488c7c3ae3f07af901dd021a0
Reed Jessen <reed@algo.ventures>
2016-04-01 12:58:56 -0700

    Initial commit of R "Hello, World!" script

---
+++ R.r
@@ -0,0 +1,20 @@
+library(httr)
+
+
+# A simple Hello World! example which requires no inputs.
+#
+# Just call hello_world() and a standard concept search query response will be pretty printed to the console.
+
+
+hello_world<- function(){
+
+    r = POST(url = "https://api.ipstreet.com/v1/sandbox/claim_only",
+        add_headers(Authorization = "sandbox_api_key"),
+        body = list(raw_text= "The quick brown fox jumps over the lazy dog"),
+        encode ="json")
+
+    print(content(r))
+
+}
+
+hello_world()
