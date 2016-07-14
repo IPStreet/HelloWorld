b4d7cec142a441623a783a4a3275212b93392293
Reed Jessen <reed@algo.ventures>
2016-07-13 17:20:57 -0700

    API now required even for sandbox

--- python.py
+++ python.py
@@ -1,37 +1,33 @@
 import requests
 import json
 
-"""
-A simple Hello World! example which requires no inputs.
-
-Just call hello_world() and a standard concept search query response will be pretty printed to the console.
-"""
-
 
 def hello_world_sandbox():
     """ Hello World! example Concept Search API call
     Prints a standard response json object when called.
-     Does not require an API Key"""
+     """
 
     endpoint = 'https://api.ipstreet.com/v1/sandbox/claim_only'
-    headers = {'x-api-key': "sandbox_api_key"}
+    headers = {'x-api-key': "Live API Key"}
     payload = json.dumps({'raw_text': 'The quick brown fox jumps over the lazy dog'})
     r = requests.post(endpoint, headers=headers, data=payload)
 
     print(json.dumps(r.json(), sort_keys=True, indent=4, separators=(',', ': ')))
     return
 
-def search_by_owner(input, page_number):
+
+def search_by_owner(owner, page_number):
     """If you have an API key, you can use this method to search for all patents owned by a given company.
     The method takes an owner name input and page number, returns target page of results from /data/patent
     """
     endpoint = 'https://api.ipstreet.com/v1/data/patent'
     headers = {'x-api-key': 'Live API Key'}
-    payload = json.dumps({'q': {'owner':input,'offset': page_number}})
+    payload = json.dumps({'q': {'owner': owner, 'offset': page_number}})
     r = requests.post(endpoint, headers=headers, data=payload)
 
-    print(r.url)
+    print(r.text)
     return r.json()
+
 
 def get_all_pages(owner):
     """When you want to get all the results of your query, you must loop over all the pages in the given results set
@@ -44,14 +40,12 @@ def get_all_pages(owner):
     # set total page count
     total_page_count = response['totalPage']
 
-
     # follow-on searches to fill data set
     current_page_count = 2  # start at page 2
     while current_page_count <= total_page_count:
         response = search_by_owner(owner, current_page_count)
 
-        #perform an insert each record into your database here
-
+        # perform an insert each record into your database here
 
         print("{} pages received".format(current_page_count))
         current_page_count += 1
@@ -59,6 +53,9 @@ def get_all_pages(owner):
     print("{} total pages received".format(current_page_count))
 
 
-
 if __name__ == '__main__':
-    hello_world_sandbox()
+    hello_world_sandbox()  
+
+    search_by_owner(owner='microsoft', page_number=1)  # get just first page of results
+
+    get_all_pages(owner='microsoft')  # get all results
