6b33f0c9626412bc05700989eab3b70c5f29079c
Reed Jessen <reed@algo.ventures>
2016-11-14 13:38:32 -0800

    Upgraded to v2.

--- R.r
+++ R.r
@@ -1,20 +1,55 @@
 library(httr)
 
-
-# A simple Hello World! example which requires no inputs.
-#
-# Just call hello_world() and a standard concept search query response will be pretty printed to the console.
-
-
-hello_world<- function(){
-
-    r = POST(url = "https://api.ipstreet.com/v1/sandbox/claim_only",
-        add_headers(`x-api-key`="sandbox_api_key"),
-        body = list(raw_text= "The quick brown fox jumps over the lazy dog"),
-        encode ="json")
-
-    print(content(r))
-
+# Utilities for getting results into data frames
+ResultsToNewDataframe <- function(results){
+  # Loop over content of response, making each a row in your data frame
+  patents <-do.call("rbind", content(results)$Assets)
+  
+  return(as.data.frame(patents)) 
 }
 
-hello_world()
+ResultsToExistingDataframe <- function(results, existing.data.frame){
+  # Append new set of results to an existing data frame of results
+  new.data.frame <-ResultsToNewDataframe(results)
+  updated.data.frame <- rbind(existing.data.frame,new.data.frame)
+  
+  
+  return(updated.data.frame) 
+}
+
+# Example IP Street API calls
+SearchByOwner <- function(owner, page.number, api.key){
+  r = POST(url = 'https://api.ipstreet.com/v2/data/patent',
+           add_headers(`x-api-key` = api.key),
+           body = list(q= list(owner = owner, page.number = page.number)),
+           encode ="json")
+  return(r)
+}
+
+
+GetFirstFourPages <- function(owner, api.key){
+  # makes the first request
+  r = SearchByOwner(owner = owner, page.number = 1, api.key = api.key)
+  
+  # create new data frame for results
+  patents = ResultsToNewDataframe(r)
+  
+  # make additional calls for each page
+  for (i in 2:4){
+    r = SearchByOwner(owner = owner, page.number = i, api.key = api.key)
+    #append results to data frame
+    patents <- ResultsToExistingDataframe(r,patents)
+  }
+  return(patents)
+}
+
+
+# Begin program run 
+api.key = 'Live_API_Key'
+
+r = SearchByOwner(owner = "microsoft", page.number = 1, api.key = api.key)
+single.page.results = ResultsToNewDataframe(r)
+
+
+first.four.pages.results = GetFirstFourPages(owner = "microsoft", api.key = api.key)
+
